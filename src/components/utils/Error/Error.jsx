d2fe1148ba1e29a0b754360c21181ba5bcd6acde
so_hell <armanyppph@gmail.com>
2023-05-06 17:40:47 +0530

    disabled eslints

--- src/components/utils/Error/Error.jsx
+++ src/components/utils/Error/Error.jsx
@@ -1,5 +1,6 @@
 import styled from "./Error.module.css";
 
+// eslint-disable-next-line react/prop-types
 const Error = ({ message }) => {
   return (
     <div className={styled["error-container"]}>
--- src/components/utils/MaxWidth/MaxWidth.jsx
+++ src/components/utils/MaxWidth/MaxWidth.jsx
@@ -1,5 +1,6 @@
 import styled from "./MaxWidth.module.css";
 
+// eslint-disable-next-line react/prop-types
 const MaxWidth = ({ maxWidth, children }) => {
   return (
     <div style={{ maxWidth: maxWidth }} className={styled["max-width"]}>
